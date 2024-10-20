41f44a7ed6792711eb8c31e92575ab76ec149a40
Theodoro Mota <theolm.mota@gmail.com>
2024-10-20 19:53:12 -0300

    Updating kotlin command

--- build_final_command.kts
+++ build_final_command.kts
@@ -1,2 +1,13 @@
 #!/usr/bin/env kotlin
-println("teste")
+
+val illegalArguments = listOf("--format", "--output", "record", "--shards", "-s", "--shard-all", "--shard-split")
+
+illegalArguments.forEach {
+    if (args[0].contains(it)) {
+        throw IllegalArgumentException("$it is not supported for the action. Please remove the $it flag from the command")
+    }
+}
+
+val input = args[0].replace("maestro", "").replace("test", "").trim()
+val finalCommand = "maestro test --format junit --output report.xml $input"
+println(finalCommand)
--- maestro.sh
+++ maestro.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-adb install ${{ inputs.apk-path }}
+APK_PATH="$1"
+RECORD="$2"
+VIDEO_RES="$3"
+BIT_RATE="$4"
+
+adb install "$APK_PATH"
+
+# Start screen recording with inputs for bit-rate and video resolution
+if [ "$RECORD" = "true" ]; then \
+  adb shell "screenrecord --bugreport --size $VIDEO_RES --bit-rate $BIT_RATE /data/local/tmp/maestro.mp4 & echo \$! > /data/local/tmp/screenrecord_pid.txt" & \
+fi
 
 # Run Maestro tests. Still needs to capture the exit status without stopping the script.
-$HOME/.maestro/bin/maestro ${{ inputs.command }}
+$HOME/.maestro/bin/$COMMAND || true
 
+# Stop screen recording and pull the video file
+if [ "$RECORD" = "true" ]; then \
+  adb shell "kill -2 \$(cat /data/local/tmp/screenrecord_pid.txt)" || true ; \
+  sleep 1 ; \
+  adb pull /data/local/tmp/maestro.mp4 /home/runner/.maestro/tests/ || true ; \
+fi
